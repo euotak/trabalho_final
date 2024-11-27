293eff24683209493ca2fe9630a8f0bb8b02d424
Bruno <123760608+euotak@users.noreply.github.com>
2024-11-27 10:21:59 -0300

    Delete trabalho_final directory

--- trabalho_final/quick_sort/quick_sort.h
+++
@@ -1,9 +0,0 @@
-#ifndef QUICK_SORT_H
-#define QUICK_SORT_H
-
-
-void trocar(int* a, int* b);
-int dividir(int* vetor, int inicio, int fim, int* contador_movimentacoes);
-void quick_sort(int* vetor, int inicio, int fim, int* contador_movimentacoes);
-
-#endif 
